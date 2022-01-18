9469e2843ff7d0b725e492009d0db2a833df95fc
Junaid <junaidrehman@devnatives.com>
2022-01-18 17:54:48 +0500

    Completed module

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Tile from '../Tile'
 import './Board.css';
 
 const Board = (props) => {
@@ -8,9 +9,11 @@ const Board = (props) => {
     gridTemplateRows: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
   }
 
-
+  const tileArray = this.props.tiles.map((tile)=>{
+      <Tile tile={...tile}/>
+  })
   return (
-    <div className='Board' style={gridConfig}>
+    <div className='Board' tileArray = {tileArray} style={gridConfig}>
     </div>
   )
 }
